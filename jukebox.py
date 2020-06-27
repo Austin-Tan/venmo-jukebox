dc1d779685883bdb23a3a2e7399720b12ec19ed0
Austin Tan <austinwtan@gmail.com>
2020-06-27 16:17:33 -0700

    Main functionality done! Working on inf while loop

--- jukebox.py
+++ jukebox.py
@@ -3,6 +3,7 @@ import pickle
 import os.path
 import base64
 import json
+import time
 
 import spotipy
 import authentication
@@ -13,7 +14,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.labels', 'https://www.googleapis.com/auth/gmail.modify']
 
 # target email address. 'me' also works for userId fields
 USERID = authentication.EMAIL
@@ -25,7 +26,10 @@ LABELID = authentication.REQUESTS_LABEL
 # max length of songs to queue in ms. (isn't she lovely is 394000)
 MAXLENGTH = 395000
 
+SPOTIFY_SERVICE = None
+
 def main():
+    global SPOTIFY_SERVICE
     """Shows basic usage of the Gmail API.
     Lists the user's Gmail labels.
     """
@@ -54,25 +58,54 @@ def main():
     # spotify authentication
     scope = "streaming user-modify-playback-state user-read-playback-state user-read-private" # scope of authority
     # sp is top-level resource for spotify api after authorizing
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, username=authentication.USERNAME, client_id=authentication.CLIENT_ID, client_secret=authentication.CLIENT_SECRET, redirect_uri = authentication.REDIRECT_URI))
+    SPOTIFY_SERVICE = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, username=authentication.USERNAME, client_id=authentication.CLIENT_ID, client_secret=authentication.CLIENT_SECRET, redirect_uri = authentication.REDIRECT_URI))
 
-    spotify_test(sp)
-    # messages_service(service)
+    # spotify_test(SPOTIFY_SERVICE)
     # original_request(service)
+
+    while True:
+        messages_service(service)
+        print("No new requests found, sleeping for another 5 seconds...")
+        time.sleep(5)
+
+
+# formats and prints a song name and artist
+def format_song(song):
+    title = song['name']
+    artist = song['artists'][0]['name']
+    millis = song['duration_ms']
+    millis = int(millis)
+    seconds = (millis/1000) % 60
+    seconds = int(seconds)
+    minutes = (millis/(1000*60)) % 60
+    minutes = int(minutes)
+    return "" + str(title) + " by " + str(artist) + " - " + str(minutes) + ":" + str(seconds)
 
 
 # test method for spotipy library
-def spotify_test(service):
-    queue_song(service, "in my life")
+# def spotify_test(service):
+    # pretty_print(service.current_playback())
+    # device_id = service.current_playback()['device']['id']
+    # print("Id is " + str(device_id))
+    # pretty_print(service.devices())
+    # queue_song("in my life")
     # queue_song(service, "podcast")
-    if not is_playing(service):
-        service.next_track()
+    # if not is_playing(service):
+    service.next_track()
 
 # tries to search for passed song.
 # if found, queue and return true.
 # if not found, return false.
-def queue_song(service, song):
-    print("Attempting to queue " + str(song))
+def queue_song(song):
+    import re
+    global SPOTIFY_SERVICE
+    service = SPOTIFY_SERVICE
+
+    if re.sub(r'\W+', '', song) == "skip":
+        service.next_track(device_id=authentication.DEVICE_ID)
+        print("Skipping current track...")
+        return True
+
     res = service.search(q=song, type='track', market='US')
     count = res['tracks']['total']
 
@@ -80,14 +113,18 @@ def queue_song(service, song):
     i = 0
     while i < total:
         cur = res['tracks']['items'][i]
-        pretty_print(cur)
+        # pretty_print(cur)
         if cur['duration_ms'] <= MAXLENGTH and cur['is_playable']:
-            service.add_to_queue(cur['uri'])
+            service.add_to_queue(cur['uri'], authentication.DEVICE_ID)
+            if not is_playing(service):
+                service.next_track()
+
+            print("Queueing " + format_song(cur))
             return True
 
     # no songs found
+    print("Could not find " + str(song))
     return False
-
 
 # return true if currently playing a song, else false
 def is_playing(service):
@@ -95,25 +132,26 @@ def is_playing(service):
         return False
     return service.current_playback()['is_playing']
 
-
-
 # near-top level helper method to look through emails that have been labeled
 # "Requests". i've set up a filter on gmail to categorize payments already
-# calls get_message to retrieve contents once located
+# calls process_message to retrieve contents once located
 def messages_service(service):
     msg_resource = service.users().messages()
     res = msg_resource.list(userId='me', labelIds = LABELID).execute()
     if res == None:
         print("ERROR IN EXECUTING LIST REQUEST")
         return
+    # pretty_print(res)
+    if res['resultSizeEstimate'] == 0: # no matching emails
+        return
     messages = res['messages']
     for message in messages:
         # print("Message Ids: " + str(message))
-        get_message(msg_resource, message['id'])
+        process_message(msg_resource, message['id'])
 
 # from a message/email id, return the contents. 
 # this extracts the html, needs get_note to extract the payment comment/note
-def get_message(resource, message_id):
+def process_message(resource, message_id):
     message = resource.get(userId='me', id=message_id).execute()
     if message == None:
         print("Message id " + str(message_id) + "invalid!!")
@@ -125,6 +163,12 @@ def get_message(resource, message_id):
     note = get_note(base64.urlsafe_b64decode(body.get("data")))
     # print(note)
     make_requests(coins, note)
+    unlabel_message(resource, message_id)
+
+# removes the "Requests" label from the message since we've processed it already
+def unlabel_message(resource, message_id):
+    new_body = {"addLabelIds":[authentication.COMPLETED_LABEL,], "removeLabelIds":[authentication.REQUESTS_LABEL,]}
+    resource.modify(userId='me', id=message_id, body=new_body).execute()
 
 # method queues a number of requests from the note given the coins inserted
 # coins is a tuple(# of nickels inserted, # of cents over-paid)
@@ -134,24 +178,45 @@ def make_requests(coins, note):
     refund = coins[1]
     requests = note
     print("Handling transaction with " + str(permitted) + " permitted requests, " + str(refund) + " too many cents, and songs:\n" + requests)
-
-    delimiters = ("<br />", ",")
+    delimiters = ("<br />", ",")    
     # loop for if we have multiple requests. try to find a delimiter
     while permitted > 1:
+        print("Top while loop, permitted: " + str(permitted) + " and requests: " + str(requests))
+        split = False
         for delimiter in delimiters:
-            start = requests.index(delimiter)
+            start = requests.find(delimiter)
             if start != -1:
                 song = requests[0:start]
                 # print("Song is :" + str(song))
                 permitted -= 1 if queue_song(song) else 0
                 requests = requests[start + len(delimiter):]
-                print("New requests: " + str(requests))
+                split = True
                 break
+            else:
+                print("Couldn't find delimiter " + delimiter + " in str " + str(requests))
+
+        if split == False:
+            # only here if no delimiters found
+            permitted -= 1 if queue_song(requests) else 0
+            print("Should refund " + str((permitted * 5) + refund) + " cents.")
+            return
 
     # queue the last request without looking to delimit
     if permitted == 1:
-        permitted -= 1 if queue_song(requests) else 0
-
+        if queue_song(requests):
+            permitted -= 1
+        else:
+            for delimiter in delimiters:
+                start = requests.find(delimiter)
+                if start != -1:
+                    song = requests[0:start]
+                    # print("Song is :" + str(song))
+                    permitted -= 1 if queue_song(song) else 0
+                    requests = requests[start + len(delimiter):]
+                    split = True
+                    break
+                else:
+                    print("Couldn't find delimiter " + delimiter + " in str " + str(requests))
     print("Should refund " + str((permitted * 5) + refund) + " cents.")
 
 # returns a tuple with the number of nickels paid (divides payment by $0.05)
